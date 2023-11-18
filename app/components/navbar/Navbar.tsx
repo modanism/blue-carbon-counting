5c42a3a3710645e153fd0e0ce2e653c6d9d71258
Mochammad Ramadhany <18220087@std.stei.itb.ac.id>
2023-11-18 16:55:26 +0700

    feat : navbar

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import Image from "next/image";
+import Logo from "../../assets/img/logo.png";
+import { usePathname } from "next/navigation";
+
+
+const Navbar = () => {
+    const pathname = usePathname()
+
+  return (
+    <nav className="bg-white flex flex-col gap-[20px] justify-center items-center py-[25px] shadow-lg fixed w-full ">
+      <Image alt="logo" src={Logo} />
+      <div className="flex gap-[48px]">
+        <a href="/" className={`text-neutral-10 text-[16px] cursor-pointer ${pathname == '/' ? "border-b-2 border-neutral-10" : ""}`}>Home</a>
+        <a href="/calculator" className={`text-neutral-10 text-[16px] cursor-pointer ${pathname == '/calculator' ? "border-b-2 border-neutral-10" : ""}`}>Calculator</a>
+        <a href="/articles" className={`text-neutral-10 text-[16px] cursor-pointer ${pathname == '/articles' ? "border-b-2 border-neutral-10" : ""}`}>Articles</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        neutral: {
+          1 : '#DEE5ED',
+          10: '#486284'
+        }
+      }
     },
   },
   plugins: [],
