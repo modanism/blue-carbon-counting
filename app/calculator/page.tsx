68ed11281362e4686647ab28675bac08ea9828cc
Mochammad Ramadhany <18220087@std.stei.itb.ac.id>
2023-11-23 23:58:21 +0700

    feat : show project data

--- app/calculator/page.tsx
+++ app/calculator/page.tsx
@@ -18,7 +18,7 @@ import {
   setDoc,
 } from "firebase/firestore";
 import { app, auth } from "../../lib/firebase/config"; // path to your Firebase configuration
-import { useState, useEffect } from "react";
+import { useState, useEffect, SetStateAction } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 
 type ProjectEntity = {
@@ -31,28 +31,39 @@ const Calculator = () => {
   const [projects, setProjects] = useState<ProjectEntity[]>([]);
   const [addButtonLoading, setAddButtonLoading] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [tabIndex, setTabIndex] = useState(0);
+  const [currentProject, setCurrentProject] = useState(projects[0]);
+
+  const handleTabsChange = (index: number) => {
+    setTabIndex(index);
+    setCurrentProject(projects[index]);
+  };
 
   // Function to fetch user data
   const fetchUserData = async () => {
     const db = getFirestore(app);
-    const docRef = doc(db, 'users', auth.currentUser?.uid ?? '');
+    const docRef = doc(db, "users", auth.currentUser?.uid ?? "");
 
     try {
       const docSnap = await getDoc(docRef);
       if (docSnap.exists() && docSnap.data().projects) {
         setProjects(docSnap.data().projects);
+        setCurrentProject(docSnap.data().projects[0]);
       } else {
         // Document doesn't exist or doesn't have projects, so create it with default data
-        const defaultProjects = [{
-          id: 'project-1',
-          name: 'Project 1',
-          text: 'testing',
-        }];
+        const defaultProjects = [
+          {
+            id: "project-1",
+            name: "Project 1",
+            text: "testing",
+          },
+        ];
         await setDoc(docRef, { projects: defaultProjects });
         setProjects(defaultProjects);
+        setCurrentProject(defaultProjects[0]);
       }
     } catch (error) {
-      console.error('Error fetching document: ', error);
+      console.error("Error fetching document: ", error);
     }
   };
 
@@ -60,13 +71,13 @@ const Calculator = () => {
   const addProjectToUser = async () => {
     setAddButtonLoading(true);
     const db = getFirestore(app);
-    const userDocRef = doc(db, 'users', auth.currentUser?.uid ?? '');
+    const userDocRef = doc(db, "users", auth.currentUser?.uid ?? "");
 
     // Create a new project object
     const newProject: ProjectEntity = {
       id: `project-${projects.length + 1}`,
       name: `Project ${projects.length + 1}`,
-      text: 'testing',
+      text: "testing",
     };
 
     try {
@@ -75,7 +86,7 @@ const Calculator = () => {
       });
       setProjects((prevProjects) => [...prevProjects, newProject]);
     } catch (error) {
-      console.error('Error adding project: ', error);
+      console.error("Error adding project: ", error);
     }
     setAddButtonLoading(false);
   };
@@ -91,6 +102,9 @@ const Calculator = () => {
       if (user) {
         setIsLoggedIn(true);
         fetchUserData();
+        if (projects.length > 0) {
+          setCurrentProject(projects[0]);
+        }
       } else {
         setIsLoggedIn(false);
       }
@@ -98,12 +112,11 @@ const Calculator = () => {
     return unsubscribe; // Make sure to unsubscribe on component unmount
   }, []);
 
-
   return (
-    <main className="min-h-screen items-center bg-[#FAFAFA]">
+    <main className="flex min-h-screen w-full bg-[#FAFAFA]">
       {isLoggedIn ? (
         <>
-          <section className="flex flex-col items-start w-[20vw] bg-[#EFF2F6] h-[100vh] pt-[150px] fixed">
+          <section className="flex flex-col items-start basis-1/5 bg-[#EFF2F6] h-[100vh] pt-[150px] relative">
             <div className="w-full min-h-[50px] flex justify-center items-center">
               {addButtonLoading ? (
                 <>
@@ -113,7 +126,7 @@ const Calculator = () => {
                 <>
                   <button
                     onClick={handleCreateProject}
-                    className="shadow-md py-[12px] px-[20px] bg-white rounded-[50px] flex justify-center items-center gap-[10px]"
+                    className="shadow-md transition-all hover:shadow-none py-[12px] px-[20px] bg-white rounded-[50px] flex justify-center items-center gap-[10px]"
                   >
                     <p className="text-[#11156D] text-[16px] font-semibold">
                       New Project
@@ -131,6 +144,8 @@ const Calculator = () => {
               variant="solid-rounded"
               colorScheme={"blue"}
               width={"100%"}
+              index={tabIndex}
+              onChange={handleTabsChange}
             >
               <TabList width={"100%"}>
                 {projects.map((item, id) => {
@@ -153,6 +168,11 @@ const Calculator = () => {
       ) : (
         <></>
       )}
+      <section className="basis-4/5 grow min-h-screen pt-[150px]">
+        <h1 className="text-[30px] text-[black]">
+          {currentProject ? currentProject.name : "Select a project"}
+        </h1>
+      </section>
     </main>
   );
 };
---
+++ components/intro/StepCard.tsx
@@ -0,0 +1,5 @@
+const StepCard = () => {
+    return (<>
+        
+    </>)
+}
